801bab7b1e684da1c07f47fd175e8b2dc7ed03e2
Guilherme Figueiredo <guilherme_figueiredo@outlook.com.br>
2022-07-07 18:59:35 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+function msgConsole(){
+    console.log("você está lendo isso no console");
+}
+
+msgConsole()
+
+//////
+const meuNome = "Guilherme"
+
+function Nome(nome){
+    console.log(nome);
+}
+
+Nome(meuNome)
+
+///////
+const estiloMusical = "Pop"
+
+function sobreMim(estiloMusical){
+    const nome = "Luiz"
+    const idade = 19
+
+    console.log(nome);
+    console.log(idade);
+    console.log(estiloMusical);
+}
+
+sobreMim(estiloMusical)
+
+/////////
+const musica = "Snowchild"
+function filmeMusica(musica){
+    const filme = "Conta Comigo"
+    console.log(filme);
+    console.log(musica);
+}
+
+filmeMusica(musica)
+
+const numero = 3
+
+function triplo(n){
+    return n*3
+}
+
+console.log(triplo(numero));
